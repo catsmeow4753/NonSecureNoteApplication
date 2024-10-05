08e4f44e2e34283a4fff224dbbdbc06f2e0e13ca
Hanauta <85512947+Hanautas@users.noreply.github.com>
2024-10-05 12:29:10 +0200

    Add project files.

---
+++ NonSecureNoteApplication/Pages/NoteEditor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NonSecureNoteApplication.Pages
+{
+    public class NoteEditorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
